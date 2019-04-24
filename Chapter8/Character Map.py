e2f1c755e2d070695474c38955dbce9689286e8d
pranjaj011 <49923043+pranjaj011@users.noreply.github.com>
2019-04-23 20:34:51 -0700

    Create Character Map.py

---
+++ Chapter8/Character Map.py
@@ -0,0 +1,14 @@
+x = 32
+for i in range(0,6):
+    print('chr:'+' ', end=' ')
+    for j in range(x,x+16):
+        print(chr(j)+'  ', end=' ')
+    print()   
+    print('asc:', end=' ')
+    for u in range(x,x+16):
+        if u >= 100:
+            print(str(u), end=' ')
+        if u <100:
+            print(str(u)+' ', end=' ')
+    print()
+    x += 16
